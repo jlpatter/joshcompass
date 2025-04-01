21d50d0d6c4021a53bfba9acccc5916442682ec3
Joshua Patterson <joshua.l.patterson7@gmail.com>
2025-03-31 20:34:14 -0500

    Add simple arrow

--- app/src/main/java/com/example/joshcompass/MainActivity.kt
+++ app/src/main/java/com/example/joshcompass/MainActivity.kt
@@ -131,7 +131,7 @@ fun Compass(azimuth: Float, modifier: Modifier = Modifier) {
                 modifier = modifier.align(Alignment.CenterHorizontally)
             )
             Image(
-                painter = painterResource(id = R.drawable.compass),
+                painter = painterResource(id = R.drawable.gps_arrow),
                 contentDescription = "Compass Image",
                 contentScale = ContentScale.Fit,
                 modifier = Modifier.size(300.dp).graphicsLayer(
